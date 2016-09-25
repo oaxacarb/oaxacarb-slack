bec5c99b7e9d6b02e0625c31d3e6ac87726db501
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2016-09-24 21:05:16 -0500

    Update gems Rails 5.0.0.1

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Railslink
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
     
     # Global values
     configatron.app_name = 'Oaxaca.rb Slack'
